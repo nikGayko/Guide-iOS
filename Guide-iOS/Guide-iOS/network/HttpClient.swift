c656e7847c82b88e26e74b592cfada0aebe8eda0
Nikita <admin@192.168.40.128>
2018-01-16 13:39:24 +0300

    update Http client class structure

--- Guide-iOS/Guide-iOS/network/HttpClient.swift
+++ Guide-iOS/Guide-iOS/network/HttpClient.swift
@@ -5,27 +5,45 @@ import SwiftyBeaver
 class HttpClient {
     typealias JSONCallback = ([String: Any]?) -> Void
     typealias ImageCallback = (UIImage?) -> Void
+    typealias DataCallback = (Data?) -> Void
     
     static func loadJSON(url: String, callback: @escaping JSONCallback) {
-        Alamofire.request(url).validate().responseJSON(completionHandler: { response in
+        loadData(url: url) { data in
+            guard let data = data else { return }
+            
+            if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+                callback(json)
+            } else {
+                SwiftyBeaver.warning("Couldn't cast data to json")
+                callback(nil)
+            }
+        }
+    }
+    
+    static func loadImage(url: String, callback: @escaping ImageCallback) {
+        loadData(url: url) { data in
+            guard let data = data else { return }
+            
+            if let image = UIImage(data: data) {
+                callback(image)
+            } else {
+                SwiftyBeaver.warning("Couldn't cast data to image")
+                callback(nil)
+            }
+        }
+    }
+    
+    static func loadData(url: String, callback: @escaping DataCallback) {
+        Alamofire.request(url).validate().responseData { response in
             switch response.result {
                 
-            case .success(let result):
-                SwiftyBeaver.info("\(result)")
-                if let json = result as? [String: Any] {
-                    callback(json)
-                } else {
-                    callback(nil)
-                }
-
+            case .success(let data):
+                callback(data)
+                
             case .failure(let error):
                 SwiftyBeaver.error(error)
                 callback(nil)
             }
-        })
-    }
-    
-    static func loadImage(url: String, callback: ImageCallback) {
-        
+        }
     }
 }
